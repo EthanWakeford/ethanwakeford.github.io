741b77513370e68496ea3cfeb38fa51ddee82e86
virtualblakewillis <bewillis101@gmail.com>
2022-12-14 13:09:23 -0600

    importing Phaser

--- web_flask/static/scripts/setup.js
+++ web_flask/static/scripts/setup.js
@@ -1,1 +1,30 @@
 console.log("loaded js file");
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 400,
+  physics: {
+    default: 'arcade'
+  },
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image('road', '/static/image_dump/spongebob_road.png');
+  // Preload Function
+}
+function create() {
+  platforms = this.physics.add.staticGroup();
+  platforms.create(200,390, 'road');
+  // Create Function
+}
+function update() {
+  // Update Function
+}
