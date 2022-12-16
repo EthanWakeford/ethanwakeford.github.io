1c94c470e34b507fbb00fdc4b660ceea2ddc403b
EthanWakeford <ethanwakeford@gmail.com>
2022-12-16 14:21:34 -0600

    restructed file layout, makes more sense now (this will be a long file)

--- web_flask/static/scripts/setup.js
+++ web_flask/static/scripts/setup.js
@@ -39,27 +39,40 @@ function preload () {
   this.load.image('moving', '../static/image_dump/BubbleBassSpriteMap_V2.png');
   this.load.image('box', '../static/image_dump/box.png');
   this.load.spritesheet('bubbleBass',
-  '../static/image_dump/BubbleBassSpriteMap_V2.png',
-  { frameWidth: 210, frameHeight: 226 }
+    '../static/image_dump/BubbleBassSpriteMap_V2.png',
+    { frameWidth: 210, frameHeight: 226 }
   );
   // Preload Function
 }
 function create () {
+  // keyboard controls setup
+  keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+  keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+  keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+  keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+
+  // world and camera building
   const { width, height } = this.sys.game.canvas;
-  this.add.image(width * 1.5, height, 'background').setScale(0.65);
+  this.physics.world.setBounds(0, 0, width * 3, height * 1.5);
+  this.cameras.main.setBounds(0, 0, width * 3, height * 1.5);
+  this.cameras.main.startFollow(player, true, 0.05, 0.05);
+  this.cameras.main.setDeadzone(200);
+
+  // background creation
   // background = this.add.tileSprite(width/2, height/2, width, height, 'background');
-  
+  this.add.image(width * 1.5, height, 'background').setScale(0.65);
+
   // static platforms
   platforms = this.physics.add.staticGroup();
   platforms.create(width, 885, 'road').setScale(1.2).refreshBody();
   platforms.create(400, 650, 'bubbles_platform').setScale(0.05).refreshBody();
   platforms.create(500, 550, 'bubbles_platform').setScale(0.05).refreshBody();
-  
+
   // bouncing object
   bouncy = this.physics.add.staticGroup();
   bouncy.create(800, 850, 'bubbles_platform').setScale(0.05).refreshBody();
   bouncy.create(950, 400, 'bubbles_platform').setScale(0.05).refreshBody();
-  
+
   // moving platforms
   movingPlatform = this.physics.add.image(1200, 900, 'moving').setScale(0.1).refreshBody();
   movingPlatform.setImmovable(true).setVelocity(100, -100).setMass(100000);
@@ -76,19 +89,32 @@ function create () {
       { x: -150, y: 100, duration: 4000, ease: 'Stepped' }
     ]
   });
-  
+
   // movable box
   box = this.physics.add.image(300, 500, 'box').setScale(0.1).refreshBody();
   box.setMass(100).setBounce(0).setDragX(200).setCollideWorldBounds(true);
-  
+
   // player object
   player = this.physics.add.sprite(200, 726, 'bubbleBass').setScale(0.35).refreshBody();
   player.setBounce(0.1).setCollideWorldBounds(true).setDragX(200);
   player.setMaxVelocity(200, 1500).setMass(1);
-  player.custom = {}
+  player.custom = {};
   player.custom.falling = { falling: false, height: 0, fallHeight: 0 };
-  player.custom.direction = 'right'
-  
+  player.custom.direction = 'right';
+
+  // physics set ups
+  this.physics.add.collider(player, bouncy, function () {
+    if (player.body.onFloor()) {
+      player.setVelocityY((player.y - player.custom.falling.height) * -80);
+      // player.setVelocityY(player.body.velocity.y * -1.5);
+    }
+  });
+  this.physics.add.collider(player, platforms);
+  this.physics.add.collider(player, box);
+  this.physics.add.collider(box, bouncy);
+  this.physics.add.collider(platforms, box);
+  this.physics.add.collider(player, movingPlatform);
+
   // animations creation
   this.anims.create({
     key: 'left',
@@ -127,37 +153,13 @@ function create () {
     frameRate: 6,
     repeat: 0
   });
-  keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
-  keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
-  keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
-  keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-
-  // physics set ups
-  this.physics.add.collider(player, bouncy, function () {
-    if (player.body.onFloor()) {
-      player.setVelocityY((player.y - player.custom.falling.height) * -80);
-      // player.setVelocityY(player.body.velocity.y * -1.5);
-    }
-  });
-  this.physics.add.collider(player, platforms);
-  this.physics.add.collider(player, box);
-  this.physics.add.collider(box, bouncy);
-  this.physics.add.collider(platforms, box);
-  this.physics.add.collider(player, movingPlatform);
-
-  // world and camera building
-  this.physics.world.setBounds(0, 0, width * 3, height * 1.5);
-  this.cameras.main.setBounds(0, 0, width * 3, height * 1.5);
-  this.cameras.main.startFollow(player, true, 0.05, 0.05);
-  this.cameras.main.setDeadzone(200);
-
   // Create Function
 }
 
 function update () {
   if (keyA.isDown) {
     player.setAccelerationX(-160);
-    player.custom.direction = 'left'
+    player.custom.direction = 'left';
     if (player.body.velocity.x > 0) {
       player.anims.play('right-slow', true);
     } else if (player.body.velocity.x >= -100) {
@@ -167,7 +169,7 @@ function update () {
     }
   } else if (keyD.isDown) {
     player.setAccelerationX(160);
-    player.custom.direction = 'right'
+    player.custom.direction = 'right';
     if (player.body.velocity.x < 0) {
       player.anims.play('left-slow', true);
     } else if (player.body.velocity.x <= 100) {
@@ -182,7 +184,7 @@ function update () {
     } else if (player.body.velocity.x > 0) {
       player.anims.play('right-slow', true);
     } else {
-      if (player.custom.direction == 'left') {
+      if (player.custom.direction === 'left') {
         player.anims.play('standLeft');
       } else {
         player.anims.play('standRight');
